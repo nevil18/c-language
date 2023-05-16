720c19b77d3757d62765a7668a4fcdc3c520cb9d
nevil18 <nevilanghan2005@gmail.com>
2023-05-16 19:32:20 +0530

    na |16/5/23 | CH-2

---
+++ CH-2/CC2.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+     const float a=2, b=4;
+     clrscr();
+     printf("area of rectangle:%.2f", a,b,a*b);
+   getch();
+}
---
+++ CH-2/CC3.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+
+{
+       const float a=3, b=4;
+	clrscr();
+	printf("area of rectangle:%.2f",a*b);
+	getch();
+}
